ed1e2882ab8a52072b677060eb2e0966ea439422
Maria Jessen <mjessen@dal.ca>
2020-06-06 23:52:32 -0300

    new approute

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 from book_grabber import main
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/passwordplease")
 def get_password():
     return "Here is a password! " + main()
